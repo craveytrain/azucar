0455b9c54476ef9477e25a7f380418f005ec4fe4
Mike Cravey <techie@craveytrain.com>
2013-05-28 01:15:35 -0500

    A few more tests

--- src/element.js
+++ src/element.js
@@ -3,15 +3,10 @@
 // Events
 Element.prototype.on = Element.prototype.addEventListener;
 
-// Element.prototype.trigger = function (type, data) {
-// 	var event = document.createEvent('HTMLEvents');
-// 	event.initEvent(type, true, true);
-// 	event.data = data || {};
-// 	event.eventName = type;
-// 	this.dispatchEvent(event);
-// };
-
-// // ClassList
-// ['add', 'remove', 'toggle'].forEach(function(_method) {
-// 	Element.prototype[_method + 'Class'] = ;
-// });
+Element.prototype.trigger = function (type, data) {
+	var event = document.createEvent('HTMLEvents');
+	event.initEvent(type, true, true);
+	event.data = data || {};
+	event.eventName = type;
+	this.dispatchEvent(event);
+};
--- test/element.js
+++ test/element.js
@@ -7,9 +7,19 @@ describe('Element', function() {
 		el = document.createElement('p')
 	})
 
-	describe('events', function() {
-		it('should have on()', function() {
+	describe('#on()', function() {
+		it('should exist', function() {
 			expect(el).to.respondTo('on')
+		})
+
+		it('should point to addEventListener()', function() {
+			expect(el.on).to.equal(el.addEventListener)
+		})
+	})
+
+	describe('#trigger()', function() {
+		it('should exist', function() {
+			expect(el).to.respondTo('trigger')
 		})
 	})
 })
